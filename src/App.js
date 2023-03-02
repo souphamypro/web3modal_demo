8e042a75b434e4a9aa85cb31332ae02ffecd90a7
souphamypro <souphamypro@gmail.com>
2023-03-02 18:37:39 +0700

    Primary Update of demo

---
+++ src/App.js
@@ -0,0 +1,287 @@
+import { useEffect, useState } from "react";
+import { CheckCircleIcon, WarningIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  HStack,
+  Input,
+  Select,
+  Text,
+  Tooltip,
+  VStack,
+  useDisclosure
+} from "@chakra-ui/react";
+import SelectWalletModal from "./Modal";
+import { toHex, truncateAddress } from "./utils";
+
+export default function Home() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [provider, setProvider] = useState();
+  const [account, setAccount] = useState();
+  const [signature, setSignature] = useState("");
+  const [error, setError] = useState("");
+  const [chainId, setChainId] = useState();
+  const [network, setNetwork] = useState(undefined);
+  const [message, setMessage] = useState("");
+  const [signedMessage, setSignedMessage] = useState("");
+  const [verified, setVerified] = useState();
+
+  const connectWithProvider = async (provider) => {
+    try {
+      setProvider(provider);
+      // Get accounts for connected wallet
+      const accounts = await provider.request({
+        method: "eth_requestAccounts"
+      });
+      if (accounts) {
+        setAccount(accounts[0]);
+      }
+      // Get current chain ID for connected wallet
+      const chainId = await provider.request({
+        method: "eth_chainId"
+      });
+      setChainId(Number(chainId));
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const handleNetwork = (e) => {
+    const id = e.target.value;
+    setNetwork(Number(id));
+  };
+
+  const handleInput = (e) => {
+    const msg = e.target.value;
+    setMessage(msg);
+  };
+
+  const switchNetwork = async () => {
+    try {
+      await provider.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: toHex(network) }]
+      });
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const signMessage = async () => {
+    if (!provider) return;
+    try {
+      const signature = await provider.request({
+        method: "personal_sign",
+        params: [message, account]
+      });
+      setSignedMessage(message);
+      setSignature(signature);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const verifyMessage = async () => {
+    if (!provider) return;
+    try {
+      const verify = await provider.request({
+        method: "personal_ecRecover",
+        params: [signedMessage, signature]
+      });
+      setVerified(verify.toLowerCase() === account.toLowerCase());
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const refreshState = () => {
+    setAccount("");
+    setChainId("");
+    setNetwork("");
+    setMessage("");
+    setSignature("");
+    setVerified(undefined);
+  };
+
+  const disconnect = () => {
+    refreshState();
+  };
+
+  useEffect(() => {
+    if (provider?.on) {
+      const handleAccountsChanged = (accounts) => {
+        console.log("accountsChanged", accounts);
+        if (accounts) setAccount(accounts[0]);
+      };
+
+      const handleChainChanged = (_hexChainId) => {
+        console.log("chain changed", _hexChainId);
+        setError(`Chain changed: ${_hexChainId}`);
+        setChainId(_hexChainId);
+      };
+
+      const handleDisconnect = () => {
+        console.log("disconnect", error);
+        disconnect();
+      };
+
+      provider.on("accountsChanged", handleAccountsChanged);
+      provider.on("chainChanged", handleChainChanged);
+      provider.on("disconnect", handleDisconnect);
+
+      return () => {
+        if (provider.removeListener) {
+          provider.removeListener("accountsChanged", handleAccountsChanged);
+          provider.removeListener("chainChanged", handleChainChanged);
+          provider.removeListener("disconnect", handleDisconnect);
+        }
+      };
+    }
+  }, [provider]);
+
+  return (
+    <>
+      <Text position="absolute" top={0} right="15px">
+        If you're in the sandbox, first "Open in New Window"{" "}
+        <span role="img" aria-label="up-arrow">
+          ⬆️
+        </span>
+      </Text>
+      <VStack justifyContent="center" alignItems="center" h="100vh">
+        <VStack marginBottom="10px">
+          <Text
+            margin="0"
+            lineHeight="1.15"
+            fontSize={["1.5em", "2em", "3em", "4em"]}
+            fontWeight="600"
+          >
+            Let's connect with
+          </Text>
+          <Text
+            margin="0"
+            lineHeight="1.15"
+            fontSize={["1.5em", "2em", "3em", "4em"]}
+            fontWeight="600"
+            sx={{
+              background: "linear-gradient(90deg, #1652f0 0%, #b9cbfb 70.35%)",
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent"
+            }}
+          >
+            Coinbase Wallet SDK
+          </Text>
+        </VStack>
+        <HStack>
+          {!account ? (
+            <Button onClick={onOpen}>Connect Wallet</Button>
+          ) : (
+            <Button onClick={disconnect}>Disconnect</Button>
+          )}
+        </HStack>
+        <VStack justifyContent="center" alignItems="center" padding="10px 0">
+          <HStack>
+            <Text>{`Connection Status: `}</Text>
+            {account ? (
+              <CheckCircleIcon color="green" />
+            ) : (
+              <WarningIcon color="#cd5700" />
+            )}
+          </HStack>
+
+          <Tooltip label={account} placement="right">
+            <Text>{`Account: ${truncateAddress(account)}`}</Text>
+          </Tooltip>
+          <Text>{`Network ID: ${chainId ? chainId : "No Network"}`}</Text>
+        </VStack>
+        {account && (
+          <HStack justifyContent="flex-start" alignItems="flex-start">
+            <Box
+              maxW="sm"
+              borderWidth="1px"
+              borderRadius="lg"
+              overflow="hidden"
+              padding="10px"
+            >
+              <VStack>
+                <Button onClick={switchNetwork} isDisabled={!network}>
+                  Switch Network
+                </Button>
+                <Select placeholder="Select network" onChange={handleNetwork}>
+                  <option value="1" disabled={chainId === 1}>
+                    Ethereum
+                  </option>
+                  <option value="3" disabled={chainId === 3}>
+                    Ropsten
+                  </option>
+                  <option value="4" disabled={chainId === 4}>
+                    Rinkeby
+                  </option>
+                  <option value="42" disabled={chainId === 42}>
+                    Kovan
+                  </option>
+                </Select>
+              </VStack>
+            </Box>
+            <Box
+              maxW="sm"
+              borderWidth="1px"
+              borderRadius="lg"
+              overflow="hidden"
+              padding="10px"
+            >
+              <VStack>
+                <Button onClick={signMessage} isDisabled={!message}>
+                  Sign Message
+                </Button>
+                <Input
+                  placeholder="Set Message"
+                  maxLength={20}
+                  onChange={handleInput}
+                  w="140px"
+                />
+                {signature ? (
+                  <Tooltip label={signature} placement="bottom">
+                    <Text>{`Signature: ${truncateAddress(signature)}`}</Text>
+                  </Tooltip>
+                ) : null}
+              </VStack>
+            </Box>
+            <Box
+              maxW="sm"
+              borderWidth="1px"
+              borderRadius="lg"
+              overflow="hidden"
+              padding="10px"
+            >
+              <VStack>
+                <Button onClick={verifyMessage} isDisabled={!signature}>
+                  Verify Message
+                </Button>
+                {verified !== undefined ? (
+                  verified === true ? (
+                    <VStack>
+                      <CheckCircleIcon color="green" />
+                      <Text>Signature Verified!</Text>
+                    </VStack>
+                  ) : (
+                    <VStack>
+                      <WarningIcon color="red" />
+                      <Text>Signature Denied!</Text>
+                    </VStack>
+                  )
+                ) : null}
+              </VStack>
+            </Box>
+          </HStack>
+        )}
+        <Text>{error ? error.message : null}</Text>
+      </VStack>
+      <SelectWalletModal
+        isOpen={isOpen}
+        closeModal={onClose}
+        connectWithProvider={connectWithProvider}
+      />
+    </>
+  );
+}
